6b19f3a33006fe61e07b88cbd465f39761fb0c03
leafAlif <leafalifeather@gmail.com>
2023-06-19 01:20:06 +0700

    Add Hamburger and href Generator

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -1,31 +1,53 @@
 'use client'
 
 import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
+import { Squash as Hamburger } from 'hamburger-react'
 
-const navLinks = [
-  { name: 'Home', href: '/' },
-  { name: 'Profil', href: '/profil' },
-  { name: 'Kontak', href: '/kontak' },
-  { name: 'Berita dan Pengumuman', href: '/berita-pengumuman'}
-]
+const navLinks = [];
+const pages = ['Home', 'Akademik', 'Berita dan Pengumuman', 'Layanan Online', 'Tentang I-Tech'];
+pages.forEach(link => {
+  const linkName = link;
+  let linkHref = link.replace(/\s+/g, '-').toLowerCase();
+  if (linkName === 'Home') {
+    linkHref = '/';
+  }
+  navLinks.push({ name: linkName, href: linkHref })
+});
 
-export default function Navigation() {
+function NavLinks() {
   const pathName = usePathname();
 
   return (
-    <nav className="nav">
+    <ol className='navbar-navlinks'>
       {navLinks.map((link) => {
         const isActive = pathName === link.href;
-
         return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={isActive ? 'active' : null}
-          > {link.name} </Link>
+          <ul key={link.name}>
+            <Link
+              href={link.href}
+              className={isActive ? 'active' : null}
+            > {link.name} </Link>
+          </ul>
         )
       })}
+    </ol>
+  )
+}
+
+export default function Navigation() {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <Link href={'/'} className='navbar-logo'>
+        <Image src='/i-tech-logo.png' alt='Logo STTI I-Tech' fill={true} />
+      </Link>
+
+      <Hamburger label='Buka Navigasi' toggled={isOpen} toggle={setOpen}></Hamburger>
+      <NavLinks></NavLinks>
     </nav>
   );
 }
