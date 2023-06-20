758b11b7a8d9b9d7cab9319856472a40b63a09a1
leafAlif <leafalifeather@gmail.com>
2023-06-20 23:40:50 +0700

    Added hamburger and create base styling for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,38 @@
 'use client'
 
+import './navbar.css';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
-import { Squash as Hamburger } from 'hamburger-react'
+import { Squash as Hamburger } from 'hamburger-react';
 
 const navLinks = [];
 const pages = ['Home', 'Akademik', 'Berita dan Pengumuman', 'Layanan Online', 'Tentang I-Tech'];
 pages.forEach(link => {
   const linkName = link;
-  let linkHref = link.replace(/\s+/g, '-').toLowerCase();
+  let linkHref = '/' + link.replace(/\s+/g, '-').toLowerCase();
   if (linkName === 'Home') {
     linkHref = '/';
   }
   navLinks.push({ name: linkName, href: linkHref })
 });
+console.log(navLinks);
 
-function NavLinks() {
+function NavLinks(props) {
   const pathName = usePathname();
+  const visibility = props.visibility;
+  const toggle = props.toggle;
 
   return (
-    <ol className='navbar-navlinks'>
+    <ol className='navbar-navlinks' visibility={visibility ? 'true' : null}>
       {navLinks.map((link) => {
         const isActive = pathName === link.href;
         return (
           <ul key={link.name}>
             <Link
               href={link.href}
+              onClick={() => toggle(!props.visibility)}
               className={isActive ? 'active' : null}
             > {link.name} </Link>
           </ul>
@@ -37,17 +42,17 @@ function NavLinks() {
   )
 }
 
-export default function Navigation() {
+export default function Navbar() {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <nav className="navbar">
+    <nav className='navbar'>
       <Link href={'/'} className='navbar-logo'>
         <Image src='/i-tech-logo.png' alt='Logo STTI I-Tech' fill={true} />
       </Link>
 
       <Hamburger label='Buka Navigasi' toggled={isOpen} toggle={setOpen}></Hamburger>
-      <NavLinks></NavLinks>
+      <NavLinks visibility={isOpen} toggle={setOpen}></NavLinks>
     </nav>
   );
 }
