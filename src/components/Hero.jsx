c22f28fc8a3ada60411e6ffbce7e8d92fa0da507
leafAlif <leafalifeather@gmail.com>
2023-06-28 01:14:09 +0700

    Add Basic Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import './hero.css'
+
+export default function Hero() {
+    return (
+        <div className='hero'>
+            <Image className='hero-background'
+                src='/hero-banner/1.jpeg' alt='Foto I-Tech'
+                fill={true}
+            />
+            <div className='hero-content'>
+                <h1 className='hero-header'>Selamat Datang di<br />
+                    Sekolah Tinggi Teknologi Informasi NIIT</h1>
+                <p>#JadilahGenerasiTeknologi</p>
+            </div>
+        </div>
+    )
+}
