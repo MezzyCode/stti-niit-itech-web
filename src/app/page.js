9a7359dbcb00852c658de90e36d0c5746b758cf0
leafAlif <leafalifeather@gmail.com>
2023-07-02 01:36:41 +0700

    Preparing for db

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,47 @@
-import { GenericCard, ProdiCard, SummaryCard } from "@/components/Cards"
-import Icon from "@/components/Icon"
+import { GenericCard, InfoCard, ProdiCard, SummaryCard } from '@/components/Cards';
+import Icon from '@/components/Icon';
+import Post from '@/database/post.json';
+import Info from '@/database/info.json';
 
 export default function Page() {
   return (
     <article>
       <SummaryCard
-        title={'Kenapa STTI NIIT ?'}
-        content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'}
+        title={Post[0][1].title}
+        content={Post[0][1].content}
         link={'/'} />
       <SummaryCard
         variant='right'
-        title={'Salam ?'}
-        content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'}
-        link={'/'} />
-      <ProdiCard></ProdiCard>
+        title={Post[0][2].title}
+        content={Post[0][2].content} link={'/'} />
+      <div className='prodi-cards'>
+        <ProdiCard
+          icon={<Icon name={'analytics'} />}
+          theme={Post[1][1].theme}
+          title={Post[1][1].title}
+          content={Post[1][1].content} link={'/'} />
+        <ProdiCard
+          icon={<Icon name={'app_shortcut'} />}
+          theme={Post[1][2].theme}
+          title={Post[1][2].title}
+          content={Post[1][2].content} link={'/'} />
+      </div>
       <GenericCard
-        icon={<Icon name={'school'}/>}
-        title={'Pendaftaran'}
-        description={'Persyaratan dan prosedur pendaftaran masuk I-Tech meliputi jenjang Sarjana, Magister, Doktoral dan Spesialis, Subspesialis dan Profesi.'}
-        link={'/'}
+        icon={<Icon name={'school'} />}
+        title={Post[0][3].title}
+        content={Post[0][3].content} link={'/'}
       />
+      <h2> Berita dan Pengumuman</h2>
+      {Info.map(item => {
+        return (
+          <InfoCard
+            key={item.id}
+            type={item.type}
+            title={item.title}
+            content={item.content}
+          />
+        )
+      })}
     </article>
   )
 }
